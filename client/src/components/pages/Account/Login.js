05c860536ea5a6ef81abce42162a3501cc5a4c73
Bazzomir <blagoj.jovanovski@outlook.com>
2022-01-06 10:52:00 +0100

    Login and Register complitely works.

--- client/src/components/pages/Account/Login.js
+++ client/src/components/pages/Account/Login.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from '../../Header/Navbar';
+import { api } from '../../../RESTApi/RestApi';
+import { useNavigate  } from "react-router-dom"
 
-export default function Login() {
+export default function Login(props) {
+    let navigate  = useNavigate ();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const logIn = (event) => {
+        event.preventDefault();
+        
+        const loginInfo = {
+            email: email,
+            password: password
+        }
+
+        fetch(`${api.root}/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginInfo)
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (!data.error) {
+                    alert(`Successfully logged in`)
+                    navigate("/home");
+                }
+                else
+                    throw new Error(data.message);
+            })
+            .catch(err => alert(err))
+    }
     return (
         <div className="container">
             <Navbar />
@@ -16,15 +52,15 @@ export default function Login() {
                 </div>
                 <div className="col" xs={4}>
                     <form style={{ width: "70%" }} >
-                        <div className="form-group mb-3" controlId="formGroupEmail">
+                        <div className="form-group mb-3">
                             <label>Email address</label>
-                            <input className="form-control" type="email" placeholder="user@domain.com" />
+                            <input className="form-control" type="email" placeholder="user@domain.com" value={email} onChange={e => { setEmail(e.target.value) }} />
                         </div>
-                        <div className="form-group mb-4" controlId="formGroupPassword">
+                        <div className="form-group mb-4">
                             <label>Password</label>
-                            <input className="form-control" type="password" placeholder="*****" />
+                            <input className="form-control" type="password" placeholder="*****" value={password} onChange={e => { setPassword(e.target.value) }} />
                         </div>
-                        <button variant="success" className="btn btn-success" >Log In</button>
+                        <button variant="success" className="btn btn-success" onClick={logIn}>Log In</button>
                     </form>
                 </div>
             </div>
