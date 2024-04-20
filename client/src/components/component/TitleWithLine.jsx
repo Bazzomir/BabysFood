173547c39cf1d957af0f30ddfd8da3500c5a128c
bazzomir <blagoj.jovanovski@outlook.com>
2024-04-21 00:22:18 +0200

    updated component

--- client/src/components/component/TitleWithLine.jsx
+++ client/src/components/component/TitleWithLine.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const TitleWithLine = ({ title, className }) => {
   return (
     <div className="col-12 px-0 w-100">
-      <h2 className="title">
-        {title}
-      </h2>
-      <hr className={`mt-1 ${className}`} />
+        <h2 className="title">
+          {title}
+        </h2>
+        <hr className={`mt-1 ${className}`} />
     </div>
   )
 };
