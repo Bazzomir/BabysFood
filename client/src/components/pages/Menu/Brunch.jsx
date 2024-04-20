e02134145781c81261395bb7e886779056591a57
bazzomir <blagoj.jovanovski@outlook.com>
2024-04-20 06:56:22 +0200

    brunch responsive - done

--- client/src/components/pages/Menu/Brunch.jsx
+++ client/src/components/pages/Menu/Brunch.jsx
@@ -21,20 +21,24 @@ export default function Brunch() {
 
     return (
         <div className="container" >
-            <div className="row pt-5" >
-                {/* <div className="row">
+            <div className="row pt-5 pb-6">
+                <div className="col pb-3">
+                    {/* <div className="row">
                     <h2 className="title">Brunch
                         <hr className='mt-2' />
                     </h2>
                 </div> */}
-                <TitleWithLine title="Brunch" />
-                {brunch.map(recipe => {
-                    return (
-                        <div className={`${brunch.length > 2 ? 'col-4' : 'col-6'} pb-5`} key={recipe._id}>
-                            <RecipeCard key={recipe._id} recipe={recipe} />
-                        </div>
-                    )
-                })}
+                    <TitleWithLine title="Brunch" />
+                    <div className="row row-cols-3 d-flex justify-content-around align-items-center gap-5 m-0 pt-3">
+                        {brunch.map(recipe => {
+                            return (
+                                // <div className={`${brunch.length > 2 ? 'col-4' : 'col-6'} pb-5`} key={recipe._id}>
+                                    <RecipeCard key={recipe._id} recipe={recipe} />
+                                // </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     )
