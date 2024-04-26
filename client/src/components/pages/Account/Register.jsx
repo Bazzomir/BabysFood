7f8edc0329b0ad5032c66158cf2234e07fc20bce
bazzomir <blagoj.jovanovski@outlook.com>
2024-04-26 21:21:09 +0200

    updated register and login responsive

--- client/src/components/pages/Account/Register.jsx
+++ client/src/components/pages/Account/Register.jsx
@@ -147,117 +147,119 @@ export default function Register() {
         <div className="container">
             <div className="row pt-5 pb-6">
                 {/* <div className="row pt-5">
-                <h2 className="title">Sing Up<hr className="mt-2" /></h2>
-            </div> */}
+                    <h2 className="title">Sing Up<hr className="mt-2" /></h2>
+                </div> */}
                 <TitleWithLine title="Sing Up" />
                 <div className="row pt-3 pb-6" >
-                    <div className="col-md-6 col-sm-12">
-                        <h2 className="col-12" ><span className="orangeText">Create Your</span> <span className="greyText"> Account</span></h2>
-                        {/* <p className="greyText"><img className="logo" src={logo} alt="Logo" />
-                        Wondering what to make for dinner tonight?
-                        Baby's Food Place is here to help you cook delicious meals with less stress and more joy.
-                        We offer recipes and cooking advice for home cooks, by home cooks.
-                    </p> */}
-                        <p className="greyText"><img className="logo" src={logo} alt="Main Logo" />
-                            Feeling indecisive about tonight's dinner? <br />Let Baby's Food Place take the guesswork out of mealtime and bring joy back into your kitchen!<br />Our platform is designed to inspire and empower home cooks like you to create delicious meals with ease and enjoyment. Say goodbye to mealtime stress and hello to culinary bliss as we provide you with a wide array of recipes, cooking tips, and guidance tailored specifically for home cooks, by home cooks. Whether you're a seasoned chef or just starting out on your culinary journey, our collection of tried-and-true recipes and expert advice will equip you with the tools and confidence you need to whip up memorable meals that will delight your family and friends. <br /> Join our community of passionate food enthusiasts and let's make cooking dinner a delightful experience together!
-                        </p>
-                    </div>
-                    <div className="col-md-6 col-sm-12">
-                        <div className="row d-flex justify-content-center align-items-center w-100">
-                            <form name="registerForm" className="needs-validation col-12" onSubmit={singUp} noValidate>
-                                <div className="row pb-4">
-                                    <div className="col-xl-6 col-xs-12">
-                                        <label htmlFor="first_name" className="form-label m-0">First Name</label>
-                                        <input className="form-control" type="text" placeholder="John" id="first_name" required value={FirstName}
-                                            onChange={e => { setFirstName(e.target.value) }} />
-                                        {/* {Object.keys(firstNameError).map((key) => {
+                    <h2 className="col-12" ><span className="orangeText">Create Your</span> <span className="greyText"> Account</span></h2>
+                    <div className="row flex-sm-row-reverse p-0 m-0 w-100">
+                        <div className="col-md-6 col-sm-12">
+                            <div className="row d-flex justify-content-center align-items-center">
+                                <form name="registerForm" className="needs-validation col-12" onSubmit={singUp} noValidate>
+                                    <div className="row pb-4">
+                                        <div className="col-xl-6 col-xs-12">
+                                            <label htmlFor="first_name" className="form-label m-0">First Name</label>
+                                            <input className="form-control" type="text" placeholder="John" id="first_name" required value={FirstName}
+                                                onChange={e => { setFirstName(e.target.value) }} />
+                                            {/* {Object.keys(firstNameError).map((key) => {
                                         return <div className="text-danger">{firstNameError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your first name.
+                                            <div className="invalid-feedback">
+                                                Please enter your first name.
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-xl-6 col-xs">
-                                        <label htmlFor="last_name" className="form-label m-0">Last Name</label>
-                                        <input className="form-control" type="text" placeholder="Doe" id="last_name" required value={LastName}
-                                            onChange={e => { setLastName(e.target.value) }} />
-                                        {/* {Object.keys(lastNameError).map((key) => {
+                                        <div className="col-xl-6 col-xs">
+                                            <label htmlFor="last_name" className="form-label m-0">Last Name</label>
+                                            <input className="form-control" type="text" placeholder="Doe" id="last_name" required value={LastName}
+                                                onChange={e => { setLastName(e.target.value) }} />
+                                            {/* {Object.keys(lastNameError).map((key) => {
                                         return <div className="text-danger">{lastNameError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your last name.
+                                            <div className="invalid-feedback">
+                                                Please enter your last name.
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row pb-4">
-                                    <div className="col-xl-6 col-xs">
-                                        <label htmlFor="email" className="form-label m-0">Email</label>
-                                        <input className="form-control" placeholder="user@domain.com" type="email" id="email" value={email} required
-                                            onChange={e => { setEmail(e.target.value) }} />
-                                        {/* {Object.keys(emailError).map((key) => {
+                                    <div className="row pb-4">
+                                        <div className="col-xl-6 col-xs">
+                                            <label htmlFor="email" className="form-label m-0">Email</label>
+                                            <input className="form-control" placeholder="user@domain.com" type="email" id="email" value={email} required
+                                                onChange={e => { setEmail(e.target.value) }} />
+                                            {/* {Object.keys(emailError).map((key) => {
                                         return <div className="text-danger">{emailError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your email address.
+                                            <div className="invalid-feedback">
+                                                Please enter your email address.
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="form-group col-xl-6 col-xs">
-                                        <label htmlFor="birthday" className="form-label m-0">Birthday</label>
-                                        <input className="form-control" type="date" value={birthday} id="birthday" required
-                                            onChange={e => { setBirthday(e.target.value) }} />
-                                        {/* {Object.keys(birthdayError).map((key) => {
+                                        <div className="form-group col-xl-6 col-xs">
+                                            <label htmlFor="birthday" className="form-label m-0">Birthday</label>
+                                            <input className="form-control" type="date" value={birthday} id="birthday" required
+                                                onChange={e => { setBirthday(e.target.value) }} />
+                                            {/* {Object.keys(birthdayError).map((key) => {
                                         return <div className="text-danger">{birthdayError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your birthday.
+                                            <div className="invalid-feedback">
+                                                Please enter your birthday.
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row pb-4">
-                                    <div className="form-group col-xl-6 col-xs">
-                                        <label htmlFor="password" className="form-label m-0">Password</label>
-                                        <input className="form-control" placeholder="********" type="password" id="password" value={password} required
-                                            onChange={e => { setPassword(e.target.value) }} />
-                                        {/* {Object.keys(passwordError).map((key) => {
+                                    <div className="row pb-4">
+                                        <div className="form-group col-xl-6 col-xs">
+                                            <label htmlFor="password" className="form-label m-0">Password</label>
+                                            <input className="form-control" placeholder="********" type="password" id="password" value={password} required
+                                                onChange={e => { setPassword(e.target.value) }} />
+                                            {/* {Object.keys(passwordError).map((key) => {
                                         return <div className="text-danger">{passwordError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your password.
+                                            <div className="invalid-feedback">
+                                                Please enter your password.
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="form-group col-xl-6 col-xs">
-                                        <label htmlFor="confim_password" className="form-label m-0">Repeat password</label>
-                                        <input className="form-control" placeholder="********" type="password" id="confim_password" value={confim_password} required
-                                            onChange={e => { setConfirmPassword(e.target.value) }} />
-                                        {/* {Object.keys(confirmPasswordError).map((key) => {
+                                        <div className="form-group col-xl-6 col-xs">
+                                            <label htmlFor="confim_password" className="form-label m-0">Repeat password</label>
+                                            <input className="form-control" placeholder="********" type="password" id="confim_password" value={confim_password} required
+                                                onChange={e => { setConfirmPassword(e.target.value) }} />
+                                            {/* {Object.keys(confirmPasswordError).map((key) => {
                                         return <div className="text-danger">{confirmPasswordError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            Please enter your password.
+                                            <div className="invalid-feedback">
+                                                Please enter your password.
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="form-group pb-4 col-12">
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value={agree} id="invalidCheck" required onChange={e => { setAgree(e.target.value) }} aria-label={ariaLabelText.createAccAriaLabel.AgreeTC} />
-                                        <label className="form-check-label" htmlFor="invalidCheck">
-                                            Agree to terms and conditions
-                                        </label>
-                                        {/* {Object.keys(checkBoxError).map((key) => {
+                                    <div className="form-group pb-4 col-12">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" value={agree} id="invalidCheck" required onChange={e => { setAgree(e.target.value) }} aria-label={ariaLabelText.createAccAriaLabel.AgreeTC} />
+                                            <label className="form-check-label" htmlFor="invalidCheck">
+                                                Agree to terms and conditions
+                                            </label>
+                                            {/* {Object.keys(checkBoxError).map((key) => {
                                         return <div className="text-danger">{checkBoxError[key]}</div>
                                     })} */}
-                                        <div className="invalid-feedback">
-                                            You must agree before submitting.
+                                            <div className="invalid-feedback">
+                                                You must agree before submitting.
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                {/* <div className="form-group pb-4 col-12">
+                                    {/* <div className="form-group pb-4 col-12">
                                 <button variant="success" type="submit" className="btn btn-green" aria-label={ariaLabelText.createAccAriaLabel.createAccBtnAriaLabel}>Create Account</button>
                             </div> */}
-                                <ButtonAuth classNameBtn="btn-green" ariaLabel={ariaLabelText.createAccAriaLabel.createAccBtnAriaLabel} buttonName="Create Account" />
-                            </form>
-                            {/* <h6 aria-label={ariaLabelText.createAccAriaLabel.haveAnAcc}>Have an account?</h6> */}
-                            <ButtonLink to="/login" ariaLabel={ariaLabelText.createAccAriaLabel.haveAnAcc} className="haveAnAcc" divClassName="pt-4" linkName="Have an account?" />
+                                    <ButtonAuth classNameBtn="btn-green" ariaLabel={ariaLabelText.createAccAriaLabel.createAccBtnAriaLabel} buttonName="Create Account" />
+                                </form>
+                                {/* <h6 aria-label={ariaLabelText.createAccAriaLabel.haveAnAcc}>Have an account?</h6> */}
+                                <ButtonLink to="/login" ariaLabel={ariaLabelText.createAccAriaLabel.haveAnAcc} className="haveAnAcc" divClassName="pt-4" linkName="Have an account?" />
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12">
+                            {/* <p className="greyText"><img className="logo" src={logo} alt="Logo" />
+                                Wondering what to make for dinner tonight?
+                                Baby's Food Place is here to help you cook delicious meals with less stress and more joy.
+                                We offer recipes and cooking advice for home cooks, by home cooks.
+                            </p> */}
+                            <p className="greyText"><img className="logo" src={logo} alt="Main Logo" />
+                                Feeling indecisive about tonight's dinner? <br />Let Baby's Food Place take the guesswork out of mealtime and bring joy back into your kitchen!<br />Our platform is designed to inspire and empower home cooks like you to create delicious meals with ease and enjoyment. Say goodbye to mealtime stress and hello to culinary bliss as we provide you with a wide array of recipes, cooking tips, and guidance tailored specifically for home cooks, by home cooks. Whether you're a seasoned chef or just starting out on your culinary journey, our collection of tried-and-true recipes and expert advice will equip you with the tools and confidence you need to whip up memorable meals that will delight your family and friends. <br /> Join our community of passionate food enthusiasts and let's make cooking dinner a delightful experience together!
+                            </p>
                         </div>
                     </div>
                 </div>
