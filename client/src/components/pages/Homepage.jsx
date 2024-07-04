4ef9d2a851cda13924e426b24299dd1a42f195c5
bazzomir <blagoj.jovanovski@outlook.com>
2024-07-04 23:50:38 +0200

    added a new component

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -2,46 +2,63 @@ import React, { useEffect, useState } from "react";
 import { api } from "../../RESTApi/RestApi";
 import RecipeCard from "../component/RecipeCard";
 import TitleWithLine from "../component/TitleWithLine";
-import Pagination from "../../components/component/Pagination";
+// import Pagination from "../../components/component/Pagination";
+import Loading from "../component/Loading";
 
 function Homepage() {
     const [NewRecipes, setNewRecipes] = useState([]);
     const [PopularRecipes, setPopularRecipes] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [cardsPerPage] = useState(9);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [cardsPerPage] = useState(9);
 
-    function getHomePage() {
-        setLoading(true);
-        fetch(`${api.root}/home`)
-            .then(res => res.json())
-            .then(data => {
-                setNewRecipes(data.NewRecipes);
-                setPopularRecipes(data.PopularRecipes);
-                setLoading(false);
-            })
-            .catch(err => {
-                alert(err);
-                setLoading(false);
-            });
-    }
+    // function getHomePage() {
+    //     setLoading(true);
+    //     fetch(`${api.root}/home`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setNewRecipes(data.NewRecipes);
+    //             setPopularRecipes(data.PopularRecipes);
+    //             setLoading(false);
+    //         })
+    //         .catch(err => {
+    //             alert(err);
+    //             setLoading(false);
+    //         });
+    // }
+
+    // useEffect(() => {
+    //     getHomePage();
+    // }, []);
 
     useEffect(() => {
+        const getHomePage = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(`${api.root}/home`);
+                const data = await res.json();
+                setNewRecipes(data.NewRecipes);
+                setPopularRecipes(data.PopularRecipes);
+            } catch (err) {
+                alert(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         getHomePage();
     }, []);
 
-    const indexOfLastCard = currentPage * cardsPerPage;
-    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-    const currentCards = PopularRecipes.slice(indexOfFirstCard, indexOfLastCard);
+    // const indexOfLastCard = currentPage * cardsPerPage;
+    // const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    // const currentCards = PopularRecipes.slice(indexOfFirstCard, indexOfLastCard);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    // const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    if (loading) {
-        return <h2>Loading...</h2>;
-    }
+    if (loading) { return <Loading /> }
 
     return (
-        <div className="container" loading={loading.toString()}>
+        <div className="container">
             <div className="row pt-5">
                 <div className="col pb-3">
                     <TitleWithLine title="Fresh & New" />
@@ -56,12 +73,12 @@ function Homepage() {
                 <div className="col-12 pb-3">
                     <TitleWithLine title="Most Popular Recipes" />
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 d-flex justify-content-around align-items-center gap-5 m-0 pt-3">
-                        {currentCards.map(recipe => (
+                        {PopularRecipes.map(recipe => (
                             <RecipeCard key={recipe._id} recipe={recipe} />
                         ))}
                     </div>
                 </div>
-                <Pagination cardsPerPage={cardsPerPage} totalCards={PopularRecipes.length} paginate={paginate} activePage={currentPage} />
+                {/* <Pagination cardsPerPage={cardsPerPage} totalCards={PopularRecipes.length} paginate={paginate} activePage={currentPage} /> */}
             </div>
         </div>
     );
